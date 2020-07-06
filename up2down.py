3ea762e5ee149f0a66e83d0683e3230f8bb49b09
nthlf110 <65693801+nthlf110@users.noreply.github.com>
2020-07-06 10:37:44 +0800

    Update up2down.py

--- up2down.py
+++ up2down.py
@@ -18,6 +18,7 @@ def read_from_xlsx(xls_file_name, sheet_name_in_xls, header="T"):
 
     result_data = []
     if header == "T":
+    	# 默认1行表头
         for i in range(1, data_nrows):
             result_data.append(dict(zip(data_sh.row_values(0), data_sh.row_values(i))))
     elif header == "F":
@@ -83,10 +84,21 @@ def if_pdl1(ts_id, summary):
     for i in iter(summary):
         if i['检测编号'] == ts_id:
             panel.append(i['检测项目'])
-    for i in panel:
-        if i.__contains__('PD-L1'):
-            return True
-    return False
+    if sum([i.__contains__('PD-L1') for i in panel]) != 0:
+        return True
+    else:
+        return False
+
+
+def if_msi(ts_id, summary):
+    panel = []
+    for i in iter(summary):
+        if i['检测编号'] == ts_id:
+            panel.append(i['检测项目'])
+    if sum([i.__contains__('MSI') for i in panel]) != 0:
+        return True
+    else:
+        return False
 
 
 def query_report(sales_name, dictionary):
@@ -97,20 +109,39 @@ def query_report(sales_name, dictionary):
 
 
 def get_date(date):
-    delta = datetime.timedelta(days=date)
-    output_date = datetime.datetime.strptime('1899-12-30','%Y-%m-%d')+delta
-    return datetime.datetime.strftime(output_date, '%Y/%m/%d')
+    if date == '':
+        return ''
+    else:
+        delta = datetime.timedelta(days=date)
+        output_date = datetime.datetime.strptime('1899-12-30', '%Y-%m-%d') + delta
+        return datetime.datetime.strftime(output_date, '%Y/%m/%d')
+
+
+def formatting(panel):
+    if isinstance(panel, str):
+        if panel.__contains__('NCCN'):
+            return '结直肠癌NCCN指南必选检测'
+        elif panel.__contains__('肠') and panel.__contains__('14'):
+            return '结直肠癌14驱动基因'
+        elif panel.__contains__('肠') and panel.__contains__('12'):
+            return '结直肠癌12'
+        elif sum([panel.upper().__contains__(i) for i in ['C','I','K','T']]) == 4:
+            return 'ckit'
+        else:
+            return panel
+    else:
+        return panel
 
 
 if __name__ == '__main__':
     parser = optparse.OptionParser()
     parser.add_option('-i', '--input', dest='input')
-    parser.add_option('-o', '--output', dest='output')
-    parser.add_option('-s', '--summary', dest='summary', default='../外送统计表/_6.8桐树NGS外送样本统计表.xlsx')
+    parser.add_option('-o', '--output', dest='output',
+                      default=datetime.datetime.strftime(datetime.date.today(), '%Y%m%d') + '-ffpe_raw.xlsx')
+    parser.add_option('-s', '--summary', dest='summary', default='../外送统计表/桐树NGS外送样本统计表.xlsx')
     parser.add_option('-r', '--report', dest='report', default='../外送统计表/报告分配.xlsx')
     (options, args) = parser.parse_args()
     input_xlsx = read_from_xlsx(options.input, 'Sheet1', header='T')
-    output_path = options.output
     summary_xlsx = read_from_xlsx(options.summary, '北东区', header='T')
     summary_xlsx.extend(read_from_xlsx(options.summary, '中区', header='T'))
     summary_xlsx.extend(read_from_xlsx(options.summary, '南区', header='T'))
@@ -123,9 +154,10 @@ if __name__ == '__main__':
         sample_id = i['检测编号']
         if isinstance(sample_id, (int, float)):
             sample_id = str(int(sample_id))
+        sample_id = sample_id.upper()
         sample_name = i['样本姓名']
         sample_type = i['样本类型']
-        test_panel = i['检测项目']
+        test_panel = formatting(i['检测项目'])
         DNA = i['DNA标签']
         if isinstance(DNA, float): DNA = str(int(DNA))
         RNA = i['RNA标签']
@@ -138,18 +170,29 @@ if __name__ == '__main__':
             if if_pdl1(sample_id, summary_xlsx):
                 note_2 = ''
             else:
-                note_2 = '不要PD-L1'
+                note_2 = '不要pdl1'
+            if if_msi(sample_id, summary_xlsx):
+                note_2 += ''
+            else:
+                note_2 += '不要msi'
 
             if receive_date:
-                deadline = receive_date + 7
+                deadline = receive_date + 6
             else:
                 deadline = ''
 
             output_xlsx.append(
                 dict(
-                    序号=index, 病例号='', 检测编号=sample_id.upper(), 样本姓名=sample_name, 销售=sales, 检测项目=test_panel,
+                    序号=index, 病例号='', 检测编号=sample_id, 样本姓名=sample_name, 销售=sales, 检测项目=test_panel,
                     DNA标签=DNA, RNA标签=RNA, 癌种=cancer, 样本类型=sample_type, 送检日期=get_date(delivery_date),
                     收样日期=get_date(receive_date), 最后出报告日期=get_date(deadline), 电子报告='',
                     备注1=note_1, 备注2=note_2, 备注3=note_3, 负责人=report))
-            info_to_xlsx(list(output_xlsx[0].keys()), [list(i.values()) for i in output_xlsx],
-                        options.output, 'Sheet1')
+        else:
+            output_xlsx.append(
+                dict(
+                    序号=index, 病例号='', 检测编号=sample_id, 样本姓名=sample_name, 销售='', 检测项目=test_panel,
+                    DNA标签=DNA, RNA标签=RNA, 癌种='', 样本类型=sample_type, 送检日期='',
+                    收样日期='', 最后出报告日期='', 电子报告='',
+                    备注1=note_1, 备注2='', 备注3='', 负责人=''))
+    info_to_xlsx(list(output_xlsx[0].keys()), [list(i.values()) for i in output_xlsx],
+                 options.output, 'Sheet1')
